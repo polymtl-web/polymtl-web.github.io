4c9c6a2b23cc7f3e1836ea64b744bef8a03965a9
Antoine Béland <antoine.beland@gmail.com>
2018-08-03 00:34:59 -0400

    Implemented mobile menu. Added new CSS rules for mobile nav.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,20 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  isMenuVisible = false;
+
+  /**
+   * Toggles the nav menu.
+   */
+  toggleMenu() {
+    this.isMenuVisible = !this.isMenuVisible;
+  }
+
+  /**
+   * Hides the nav menu.
+   */
+  hideMenu() {
+    this.isMenuVisible = false;
+  }
+}
